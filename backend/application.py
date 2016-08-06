720e7905f420e9f22d2552d6496f238ec4cd1077
Kyle <kylezhou2002@hotmail.com>
2016-08-06 17:00:17 -0400

    Added db stuff

--- backend/application.py
+++ backend/application.py
@@ -1,9 +1,35 @@
-from flask import Flask
+import sqlite3
+
+from flask import Flask, request, g
+
+import contracts.general as general_contracts
+import contracts.tweet as tweet_contracts
+
+DATABASE = './database.db'
+
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def home():
-	return "HAI WRLD!"
+    return "HAI WRLD!"
+
+
+@app.route('/anatweet', methods=['GET'])
+def request_tweet_data():
+    pass
+
 
 if __name__ == '__main__':
-	app.run()
+    app.run()

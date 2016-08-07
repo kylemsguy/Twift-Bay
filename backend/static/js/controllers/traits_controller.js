87ea665bff2ee6d9383e170b2ab3c91b99e71621
Sergio Flores <sergioflores836@gmail.com>
2016-08-07 04:58:23 -0500

    Added on load transition and mocked up some fake data

--- backend/static/js/controllers/traits_controller.js
+++ backend/static/js/controllers/traits_controller.js
@@ -1,6 +1,50 @@
 Traits.TraitsController = Ember.ArrayController.extend({
 	inputValue: '@',
 	displayResults: false,
+	personality: [
+				{
+					"id": "Adventurousness",
+					"sampling_error": 0.0494392844,
+					"percentage": 0.5815211780249463,
+					"name": "Adventurousness",
+					"category": "personality"
+				},
+				{
+					"id": "Artistic interests",
+					"sampling_error": 0.1009730732,
+					"percentage": 0.027685155619603698,
+					"name": "Artistic interests",
+					"category": "personality"
+				},
+				{
+					"id": "Emotionality",
+					"sampling_error": 0.0464381914,
+					"percentage": 0.21295364136485728,
+					"name": "Emotionality",
+					"category": "personality"
+				},
+				{
+					"id": "Imagination",
+					"sampling_error": 0.061762134499999996,
+					"percentage": 0.869645354683581,
+					"name": "Imagination",
+					"category": "personality"
+				},
+				{
+					"id": "Intellect",
+					"sampling_error": 0.054068624100000004,
+					"percentage": 0.8023486487481887,
+					"name": "Intellect",
+					"category": "personality"
+				},
+				{
+					"id": "Liberalism",
+					"sampling_error": 0.0812002711,
+					"percentage": 0.8810067929131393,
+					"name": "Authority-challenging",
+					"category": "personality"
+				}
+								],
 	actions: {
 		sendTwitterHandle: function(){
 			var twitterHandle = this.get("inputValue");
